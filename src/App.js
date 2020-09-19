651513da0e4a68d0c7e02b69113182b0aae91c14
Martin L <ml@weboperations.com>
2020-09-19 22:17:53 +0200

    vor Travis Einfuehrung Kap.92

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there: Edit <code>src/App.js</code> and save to reload.
+          Hi ML#!!!! Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Python or React ...
         </a>
       </header>
     </div>
